39d30b21832dd77ebbadabe81a49c51e2e3ead48
mengweijin <1002284406@qq.com>
2020-12-23 22:29:08 +0800

    update

--- src/main/java/com/github/mengweijin/generator/engine/BeetlStringTemplateEngine.java
+++ src/main/java/com/github/mengweijin/generator/engine/BeetlStringTemplateEngine.java
@@ -1,11 +1,14 @@
 package com.github.mengweijin.generator.engine;
 
+import cn.hutool.core.io.FileUtil;
 import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
 import com.baomidou.mybatisplus.generator.engine.AbstractTemplateEngine;
+import com.baomidou.mybatisplus.generator.engine.BeetlTemplateEngine;
 import org.beetl.core.Configuration;
 import org.beetl.core.GroupTemplate;
 import org.beetl.core.Template;
 import org.beetl.core.resource.StringTemplateResourceLoader;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
@@ -13,7 +16,7 @@ import java.util.Map;
 /**
  * @author mengweijin
  */
-public class BeetlStringTemplateEngine extends CustomerAbstractTemplateEngine {
+public class BeetlStringTemplateEngine extends BeetlTemplateEngine {
 
     private GroupTemplate groupTemplate;
 
@@ -22,29 +25,23 @@ public class BeetlStringTemplateEngine extends CustomerAbstractTemplateEngine {
         super.init(configBuilder);
         try {
             Configuration cfg = Configuration.defaultConfiguration();
-            StringTemplateResourceLoader resourceLoader = new StringTemplateResourceLoader();
-            groupTemplate = new GroupTemplate(resourceLoader, cfg);
+            groupTemplate = new GroupTemplate(new StringTemplateResourceLoader(), cfg);
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
         }
         return this;
     }
 
-    /**
-     *
-     * @param objectMap
-     * @param templateString template String
-     * @param outputFile
-     * @throws Exception
-     */
     @Override
-    public void writer(Map<String, Object> objectMap, String templateString, String outputFile) throws Exception {
-        Template template = groupTemplate.getTemplate(templateString);
+    public void writer(Map<String, Object> objectMap, String templatePath, String outputFile) throws Exception {
+        // read template content from template path
+        String templateContent = FileUtil.readUtf8String(templatePath);
+        Template template = groupTemplate.getTemplate(templateContent);
         try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
             template.binding(objectMap);
             template.renderTo(fileOutputStream);
         }
-        logger.debug("文件:" + outputFile);
+        logger.debug("模板:" + templatePath + ";  文件:" + outputFile);
     }
 
     @Override
